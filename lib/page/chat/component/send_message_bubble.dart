d30941db6cf82cfebaab522d29390b4e1d79d124
chocolate-kuma <natsumi0814okada@gmail.com>
2023-06-11 23:11:38 +0900

    change style

--- lib/page/chat/component/send_message_bubble.dart
+++ lib/page/chat/component/send_message_bubble.dart
@@ -13,23 +13,23 @@ class SendMessageBubble extends StatelessWidget {
         alignment: Alignment.centerRight,
         child: Container(
           decoration: const BoxDecoration(
-            boxShadow: [
-              BoxShadow(
-                color: ColorConstant.black60,
-                spreadRadius: 0.1,
-                blurRadius: 1,
-              )
-            ],
-            borderRadius: BorderRadius.only(
-              topRight: Radius.circular(40),
-              topLeft: Radius.circular(40),
-              bottomLeft: Radius.circular(40),
-            ),
-            color: ColorConstant.purple95
-          ),
+              boxShadow: [
+                BoxShadow(
+                  color: ColorConstant.black60,
+                  spreadRadius: 0.1,
+                  blurRadius: 1,
+                )
+              ],
+              borderRadius: BorderRadius.all(
+                Radius.circular(24),
+              ),
+              color: ColorConstant.secondary),
           child: Padding(
             padding: const EdgeInsets.all(16.0),
-            child: Text(message),
+            child: Text(
+              message,
+              style: TextStyle(fontSize: 16),
+            ),
           ),
         ),
       ),
