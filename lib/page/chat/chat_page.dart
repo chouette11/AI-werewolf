8f0e1b16814441d3b8d944473f64d04a84cae071
chouette11 <keifukuda1115@gmail.com>
2023-06-11 23:11:52 +0900

    Apply message entity

--- lib/page/chat/chat_page.dart
+++ lib/page/chat/chat_page.dart
@@ -1,29 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:wordwolf/page/chat/component/bottom_text_field.dart';
 import 'package:wordwolf/page/chat/component/send_message_bubble.dart';
+import 'package:wordwolf/provider/presentation_providers.dart';
 
 import 'component/receive_message_bubble.dart';
 
-class ChatPage extends StatelessWidget {
+class ChatPage extends ConsumerWidget {
   const ChatPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final myOpp = ["aaaa", "bbbb", "cccc"];
-    final oppMy = ["dddd", "eeee", "ffff"];
+  Widget build(BuildContext context, WidgetRef ref) {
+    final messages = ref.watch(messagesProvider);
     return Scaffold(
       appBar: AppBar(title: const Text("お題はうどん")),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          ref.read(messagesProvider.notifier).addMessage("aaaa");
+        },
+        child: const Icon(Icons.add),
+      ),
       bottomSheet: const BottomTextField(),
       body: ListView.builder(
         reverse: true,
-        itemCount: myOpp.length + oppMy.length,
+        itemCount: messages.length,
         itemBuilder: (BuildContext context, int index) {
-          final messages = [...myOpp, ...oppMy];
+          messages.sort((a, b) {
+            //sorting in descending order
+            return b.createdAt.compareTo(a.createdAt);
+          });
           final message = messages[index];
-          if (index > 2) {
-            return SendMessageBubble(message: message);
+          if (message.userId == "my") {
+            return SendMessageBubble(message: message.content);
           } else {
-            return ReceiveMessageBubble(message: message);
+            return ReceiveMessageBubble(message: message.content);
           }
         },
       ),
