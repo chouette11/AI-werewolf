31ed44e63f75f2318f52a8659da270ac41b86d46
chouette11 <keifukuda1115@gmail.com>
2023-06-12 00:17:16 +0900

    Fix messages provider

--- lib/provider/presentation_providers.dart
+++ lib/provider/presentation_providers.dart
@@ -1,21 +1,8 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
-import 'package:wordwolf/entity/task/message_entity.dart';
-
-part 'presentation_providers.g.dart';
-
-@riverpod
-class Messages extends _$Messages {
-  @override
-  List<MessageEntity> build() {
-    return [];
-  }
-
-  // Let's allow the UI to add messages.
-  void addMessage(String message) {
-    final entity = MessageEntity(content: "${state.length}", userId: state.length == 3 ? "my" : "other", createdAt: DateTime.now());
-    state = [...state, entity];
-  }
-}
+import 'package:wordwolf/repository/task_repository.dart';
 
 final exampleTextFieldProvider = StateProvider<String>((ref) => '');
+
+final messagesStreamProvider = StreamProvider(
+  (ref) => ref.watch(messageRepositoryProvider).getMessageStream("0000"),
+);
