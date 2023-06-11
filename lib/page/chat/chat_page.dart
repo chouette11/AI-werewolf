0a63d3ef61739f605e783461c17ca07614ceac3b
chouette11 <keifukuda1115@gmail.com>
2023-06-12 00:18:17 +0900

    Apply message stream provider

--- lib/page/chat/chat_page.dart
+++ lib/page/chat/chat_page.dart
@@ -1,40 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:wordwolf/data/firestore_data_source.dart';
+import 'package:wordwolf/document/message/message_document.dart';
 import 'package:wordwolf/page/chat/component/bottom_text_field.dart';
+import 'package:wordwolf/page/chat/component/receive_message_bubble.dart';
 import 'package:wordwolf/page/chat/component/send_message_bubble.dart';
 import 'package:wordwolf/provider/presentation_providers.dart';
 
-import 'component/receive_message_bubble.dart';
 
 class ChatPage extends ConsumerWidget {
   const ChatPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final messages = ref.watch(messagesProvider);
+    final messages = ref.watch(messagesStreamProvider);
     return Scaffold(
       appBar: AppBar(title: const Text("お題はうどん")),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          ref.read(messagesProvider.notifier).addMessage("aaaa");
+          ref.read(firestoreProvider).insertMessage(
+              MessageDocument(
+                  content: "content",
+                  userId: "my",
+                  createdAt: DateTime.now()),
+              "0000");
+          // ref.read(messagesProvider.notifier).addMessage("aaaa");
         },
         child: const Icon(Icons.add),
       ),
       bottomSheet: const BottomTextField(),
-      body: ListView.builder(
-        reverse: true,
-        itemCount: messages.length,
-        itemBuilder: (BuildContext context, int index) {
-          messages.sort((a, b) {
-            //sorting in descending order
-            return b.createdAt.compareTo(a.createdAt);
-          });
-          final message = messages[index];
-          if (message.userId == "my") {
-            return SendMessageBubble(message: message.content);
-          } else {
-            return ReceiveMessageBubble(message: message.content);
-          }
+      body: messages.when(
+        data: (data) {
+          return ListView.builder(
+            reverse: true,
+            itemCount: data.length,
+            itemBuilder: (BuildContext context, int index) {
+              data.sort((a, b) {
+                //sorting in descending order
+                return b.createdAt.compareTo(a.createdAt);
+              });
+              final message = data[index];
+              if (message.userId == "my") {
+                return SendMessageBubble(message: message.content);
+              } else {
+                return ReceiveMessageBubble(message: message.content);
+              }
+            },
+          );
+        },
+        error: (error, stackTrace) {
+          return Text(error.toString());
+        },
+        loading: () {
+          return const CircularProgressIndicator();
         },
       ),
     );
