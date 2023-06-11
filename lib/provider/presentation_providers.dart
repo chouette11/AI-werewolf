c1cd615c888bc1dd2bdac771856b7ef8c5848b7f
chouette11 <keifukuda1115@gmail.com>
2023-06-11 23:11:33 +0900

    Add messages provider

--- lib/provider/presentation_providers.dart
+++ lib/provider/presentation_providers.dart
@@ -1,3 +1,21 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:wordwolf/entity/task/message_entity.dart';
+
+part 'presentation_providers.g.dart';
+
+@riverpod
+class Messages extends _$Messages {
+  @override
+  List<MessageEntity> build() {
+    return [];
+  }
+
+  // Let's allow the UI to add messages.
+  void addMessage(String message) {
+    final entity = MessageEntity(content: "${state.length}", userId: state.length == 3 ? "my" : "other", createdAt: DateTime.now());
+    state = [...state, entity];
+  }
+}
 
 final exampleTextFieldProvider = StateProvider<String>((ref) => '');
