6869bdbb2d4212becec80a12d0d79ddf6f21659f
chocolate-kuma <natsumi0814okada@gmail.com>
2023-06-12 15:05:53 +0900

    add meke dialog

--- lib/page/root/root_page.dart
+++ lib/page/root/root_page.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:go_router/go_router.dart';
 import 'package:wordwolf/constant/color_constant.dart';
 import 'package:wordwolf/constant/text_style_constant.dart';
 
 class RootPage extends StatelessWidget {
   const RootPage({super.key});
+  final String id = "1234567890";
 
   @override
   Widget build(BuildContext context) {
@@ -17,26 +19,7 @@ class RootPage extends StatelessWidget {
               "ワードウルフ",
               style: TextStyleConstant.bold28,
             ),
-            SizedBox(height: 40),
-            SizedBox(
-              height: 48,
-              width: 160,
-              child: ElevatedButton(
-                onPressed: () => context.push("/chat"),
-                child: const Text(
-                  "部屋作成",
-                  style: TextStyleConstant.white24,
-                ),
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: ColorConstant.main,
-                  backgroundColor: ColorConstant.main,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16),
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 16),
+            const SizedBox(height: 40),
             SizedBox(
               height: 48,
               width: 160,
@@ -50,10 +33,111 @@ class RootPage extends StatelessWidget {
                         shape: const RoundedRectangleBorder(
                           borderRadius: BorderRadius.all(Radius.circular(16)),
                         ),
-                        content: Container(
+                        content: SizedBox(
                           width: 240,
                           height: 160,
-                          child: const Column(
+                          child: Column(
+                            children: [
+                              const Text(
+                                'ID',
+                                style: TextStyleConstant.normal32,
+                              ),
+                              const SizedBox(height: 16),
+                              Text(
+                                id,
+                                style: TextStyleConstant.normal32,
+                              ),
+                              const SizedBox(height: 56),
+                            ],
+                          ),
+                        ),
+                        actions: <Widget>[
+                          Center(
+                            child: SizedBox(
+                              height: 48,
+                              width: 120,
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  Clipboard.setData(ClipboardData(text: id));
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                      content: Text('テキストがクリップボードに保存されました'),
+                                    ),
+                                  );
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: ColorConstant.accent,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(16),
+                                  ),
+                                ),
+                                child: Text(
+                                  "コピー",
+                                  style: TextStyleConstant.normal16.copyWith(
+                                    color: ColorConstant.base,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 24),
+                          Center(
+                            child: SizedBox(
+                              height: 48,
+                              width: 120,
+                              child: ElevatedButton(
+                                onPressed: () => context.push("/chat"),
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: ColorConstant.accent,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(16),
+                                  ),
+                                ),
+                                child: Text(
+                                  "参加する",
+                                  style: TextStyleConstant.normal16.copyWith(
+                                    color: ColorConstant.base,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: ColorConstant.main,
+                  backgroundColor: ColorConstant.main,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
+                child: const Text(
+                  "部屋作成",
+                  style: TextStyleConstant.white24,
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
+            SizedBox(
+              height: 48,
+              width: 160,
+              child: ElevatedButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (_) {
+                      return AlertDialog(
+                        backgroundColor: ColorConstant.secondary,
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(16)),
+                        ),
+                        content: const SizedBox(
+                          width: 240,
+                          height: 160,
+                          child: Column(
                             children: [
                               Text(
                                 '参加ID',
@@ -104,9 +188,11 @@ class RootPage extends StatelessWidget {
                                     borderRadius: BorderRadius.circular(16),
                                   ),
                                 ),
-                                child: const Text(
+                                child: Text(
                                   "参加する",
-                                  style: TextStyleConstant.white16,
+                                  style: TextStyleConstant.normal16.copyWith(
+                                    color: ColorConstant.base,
+                                  ),
                                 ),
                               ),
                             ),
@@ -116,16 +202,16 @@ class RootPage extends StatelessWidget {
                     },
                   );
                 },
-                child: const Text(
-                  "参加する",
-                  style: TextStyleConstant.normal24,
-                ),
                 style: ElevatedButton.styleFrom(
                   foregroundColor: ColorConstant.secondary,
                   backgroundColor: ColorConstant.secondary,
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(16),
                   ),
+                ),
+                child: const Text(
+                  "参加する",
+                  style: TextStyleConstant.normal24,
                 ),
               ),
             ),
