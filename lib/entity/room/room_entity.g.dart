7ef8a6a8a79f36ea750569fd4ab226662bc541dc
chouette11 <keifukuda1115@gmail.com>
2023-06-12 20:15:11 +0900

    Add room entity

---
+++ lib/entity/room/room_entity.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'room_entity.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_RoomEntity _$$_RoomEntityFromJson(Map<String, dynamic> json) =>
+    _$_RoomEntity(
+      id: json['id'] as String,
+    );
+
+Map<String, dynamic> _$$_RoomEntityToJson(_$_RoomEntity instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+    };
