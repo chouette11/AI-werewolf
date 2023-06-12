79c7fa454d13950bbcf1a290b73f1908b9180f9f
chouette11 <keifukuda1115@gmail.com>
2023-06-12 18:34:26 +0900

    Ad message from gpt

--- lib/repository/message_repository.dart
+++ lib/repository/message_repository.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:wordwolf/data/api_data_source.dart';
 import 'package:wordwolf/data/firestore_data_source.dart';
 import 'package:wordwolf/entity/task/message_entity.dart';
 
@@ -14,8 +15,17 @@ class MessageRepository {
   Future<void> addMessage(String content, String roomId) async {
     final firestore = ref.read(firestoreProvider);
     final entity = MessageEntity(content: content, userId: "userId", createdAt: DateTime.now());
-    final messageDoc = await entity.toMessageDocument();
+    final messageDoc = entity.toMessageDocument();
     await firestore.insertMessage(messageDoc, roomId);
+  }
+
+  Future<void> addMessageFromGpt(String topic, String roomId) async {
+    final api = ref.read(apiProvider);
+    final firestore = ref.read(firestoreProvider);
+    final message = await api.fetchMessage(topic);
+    final entity = MessageEntity(content: message.content, userId: message.userId, createdAt: DateTime.now());
+    final messageDoc = entity.toMessageDocument();
+    await firestore.insertMessage(messageDoc, roomId); 
   }
 
   /// タスクのストリームを取得
