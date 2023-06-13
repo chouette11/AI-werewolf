d0e6e0ad86c669985ba081360f6cb4a9e846be02
chouette11 <keifukuda1115@gmail.com>
2023-06-13 10:20:49 +0900

    Add get romm member stream

--- lib/repository/room_repository.dart
+++ lib/repository/room_repository.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:wordwolf/data/firestore_data_source.dart';
 import 'package:wordwolf/entity/room/room_entity.dart';
@@ -11,17 +13,29 @@ class RoomRepository {
 
   final Ref ref;
 
-  /// 新規タスク追加
-  Future<void> makeRoom(String roomId) async {
+  /// 新規ルーム追加
+  Future<void> makeRoom(String roomId, int maxNum) async {
     final firestore = ref.read(firestoreProvider);
-    final entity = RoomEntity(id: roomId);
+    final rng = Random();
+    final memberId = rng.nextInt(maxNum);
+    final entity =
+        RoomEntity(id: roomId, members: {'gpt': memberId}, maxNum: maxNum);
     final roomDoc = entity.toRoomDocument();
     await firestore.createRoom(roomDoc);
   }
 
+  /// ルームに参加
   Future<void> joinRoom(String roomId) async {
     final firestore = ref.read(firestoreProvider);
     final uid = ref.read(uidProvider);
     await firestore.addMemberToRoom(roomId, uid);
+  }
+
+  /// ルームメンバーのストリームを取得
+  Stream<Map<String, int>> getRoomMemberStream(String roomId) {
+    final firestore = ref.read(firestoreProvider);
+    return firestore.fetchRoomStream(roomId).map(
+          (event) => RoomEntity.fromDoc(event).members,
+        );
   }
 }
