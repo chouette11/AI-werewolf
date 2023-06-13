65f339145ece2e3931bcf2f6b7eacb3453f1bb74
chouette11 <keifukuda1115@gmail.com>
2023-06-13 09:05:19 +0900

    Add join room

--- lib/repository/room_repository.dart
+++ lib/repository/room_repository.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:wordwolf/data/firestore_data_source.dart';
 import 'package:wordwolf/entity/room/room_entity.dart';
+import 'package:wordwolf/provider/presentation_providers.dart';
 
 final roomRepositoryProvider =
     Provider<RoomRepository>((ref) => RoomRepository(ref));
@@ -16,5 +17,11 @@ class RoomRepository {
     final entity = RoomEntity(id: roomId);
     final roomDoc = entity.toRoomDocument();
     await firestore.createRoom(roomDoc);
+  }
+
+  Future<void> joinRoom(String roomId) async {
+    final firestore = ref.read(firestoreProvider);
+    final uid = ref.read(uidProvider);
+    await firestore.addMemberToRoom(roomId, uid);
   }
 }
