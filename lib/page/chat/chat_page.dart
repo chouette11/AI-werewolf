e765fdde56d12e42423386ce5770670cd962ba80
chouette11 <keifukuda1115@gmail.com>
2023-06-13 22:44:25 +0900

    Add roomId to arguments

--- lib/page/chat/chat_page.dart
+++ lib/page/chat/chat_page.dart
@@ -41,6 +41,7 @@ class _ChatPageState extends ConsumerState<ChatPage> {
                   context: context,
                   builder: (context) => AnswerDialog(
                     memberMap: data,
+                    roomId: widget.roomId,
                   ),
                 );
               },
