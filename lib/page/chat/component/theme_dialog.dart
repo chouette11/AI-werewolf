fb55ebb7559b5d508fc3e9e7be24d525230008ca
chouette11 <keifukuda1115@gmail.com>
2023-06-14 22:25:35 +0900

    Add logic

--- lib/page/chat/component/theme_dialog.dart
+++ lib/page/chat/component/theme_dialog.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:wordwolf/constant/color_constant.dart';
 import 'package:wordwolf/constant/text_style_constant.dart';
+import 'package:wordwolf/provider/presentation_providers.dart';
+import 'package:wordwolf/repository/message_repository.dart';
 
-class ThemeDialog extends StatelessWidget {
-  const ThemeDialog({super.key});
+class ThemeDialog extends ConsumerStatefulWidget {
+  const ThemeDialog(this.roomId, this.maxNum, {super.key});
+  final String roomId;
+  final int maxNum;
+
+  @override
+  ConsumerState<ThemeDialog> createState() => _ThemeDialogState();
+}
+
+class _ThemeDialogState extends ConsumerState<ThemeDialog> {
+  bool isSend = false;
 
   @override
   Widget build(BuildContext context) {
+    final messages = ref.watch(messagesStreamProvider(widget.roomId));
     return AlertDialog(
       backgroundColor: ColorConstant.base,
       shape: const RoundedRectangleBorder(
@@ -16,68 +29,92 @@ class ThemeDialog extends StatelessWidget {
         height: 360,
         width: 240,
         child: Center(
-          child: Column(
-            children: [
-              const Text('あなたは', style: TextStyleConstant.normal16),
-              const SizedBox(height: 8),
-              const Text('ユーザー1(一般人)', style: TextStyleConstant.bold24),
-              const SizedBox(height: 16),
-              const Row(
+          child: messages.when(
+            data: (data) {
+              return Column(
                 children: [
-                  Spacer(),
-                  Text('お題', style: TextStyleConstant.normal18),
-                  Spacer(),
-                  Text('うどん', style: TextStyleConstant.bold28),
-                  Spacer(),
+                  const Text('あなたは', style: TextStyleConstant.normal16),
+                  const SizedBox(height: 8),
+                  const Text('ユーザー1(一般人)', style: TextStyleConstant.bold24),
+                  const SizedBox(height: 16),
+                  const Row(
+                    children: [
+                      Spacer(),
+                      Text('お題', style: TextStyleConstant.normal18),
+                      Spacer(),
+                      Text('うどん', style: TextStyleConstant.bold28),
+                      Spacer(),
+                    ],
+                  ),
+                  const SizedBox(height: 32),
+                  SizedBox(
+                    height: 100,
+                    width: 200,
+                    child: TextField(
+                      onChanged: (value) => ref
+                          .read(startTextFieldProvider.notifier)
+                          .update((state) => value),
+                      cursorColor: ColorConstant.main,
+                      keyboardType: TextInputType.multiline,
+                      maxLines: null,
+                      decoration: InputDecoration(
+                        enabledBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: ColorConstant.main,
+                          ),
+                        ),
+                        focusedBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: ColorConstant.main,
+                          ),
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                        hintText: '解答を入力してください',
+                        hintStyle: TextStyleConstant.normal16,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 12),
+                  Visibility(
+                    visible: data.length != widget.maxNum && isSend,
+                    child: const Text(
+                      '全員が送信するまでしばらくお待ち下さい...',
+                      style: TextStyleConstant.normal12,
+                    ),
+                  ),
+                  const SizedBox(height: 12),
+                  SizedBox(
+                    height: 48,
+                    width: 80,
+                    child: ElevatedButton(
+                      onPressed: isSend ? null : () {
+                        final value = ref.read(startTextFieldProvider);
+                        ref
+                            .read(messageRepositoryProvider)
+                            .addMessage(value, widget.roomId);
+                        isSend = true;
+                        setState(() {});
+                      }, //textfieldに入力された値を送信する
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: ColorConstant.main,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                      ),
+                      child: Text(
+                        '送信',
+                        style: TextStyleConstant.bold14
+                            .copyWith(color: ColorConstant.black100),
+                      ),
+                    ),
+                  ),
                 ],
-              ),
-              const SizedBox(height: 32),
-              SizedBox(
-                height: 100,
-                width: 200,
-                child: TextField(
-                  cursorColor: ColorConstant.main,
-                  keyboardType: TextInputType.multiline,
-                  maxLines: null,
-                  decoration: InputDecoration(
-                    enabledBorder: const OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: ColorConstant.main,
-                      ),
-                    ),
-                    focusedBorder: const OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: ColorConstant.main,
-                      ),
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(16),
-                    ),
-                    hintText: '解答を入力してください',
-                    hintStyle: TextStyleConstant.normal16,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 24),
-              SizedBox(
-                height: 48,
-                width: 80,
-                child: ElevatedButton(
-                  onPressed: () {}, //textfieldに入力された値を送信する
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: ColorConstant.main,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16),
-                    ),
-                  ),
-                  child: Text(
-                    '送信',
-                    style: TextStyleConstant.bold14
-                        .copyWith(color: ColorConstant.black100),
-                  ),
-                ),
-              ),
-            ],
+              );
+            },
+            error: (_, __) => const Text('error'),
+            loading: () => const CircularProgressIndicator(),
           ),
         ),
       ),
