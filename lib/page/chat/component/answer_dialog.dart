d06b51cd1d865ad227cb68d567778b0cc7a2feed
chocolate-kuma <natsumi0814okada@gmail.com>
2023-06-14 18:13:03 +0900

    fix listtile

--- lib/page/chat/component/answer_dialog.dart
+++ lib/page/chat/component/answer_dialog.dart
@@ -7,72 +7,75 @@ import 'package:wordwolf/provider/presentation_providers.dart';
 class AnswerDialog extends ConsumerWidget {
   const AnswerDialog({
     super.key,
-    required this.memberMap,
     required this.roomId,
   });
 
-  final Map<String, int> memberMap;
   final String roomId;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final value = ref.watch(answerRadioValueProvider);
+    final members = ref.watch(membersStreamProvider(roomId));
 
-    List<Widget> listTiles() {
-      final List<Widget> tiles = [];
-      final nameList = memberMap.values.toList();
-      for (var name in nameList) {
-        tiles.add(ListTile(
-          title: Text(name.toString()),
-          leading: Radio<String>(
-            activeColor: ColorConstant.main,
-            value: name.toString(),
-            groupValue: value,
-            onChanged: (String? value) {
-              if (value != null) {
-                ref
-                    .read(answerRadioValueProvider.notifier)
-                    .update((state) => value);
-              }
-            },
+    return members.when(
+      data: (data) {
+        final nameList = data.values.toList();
+        return AlertDialog(
+          backgroundColor: ColorConstant.black100,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(16)),
           ),
-        ));
-      }
-      return tiles;
-    }
-
-    return AlertDialog(
-      backgroundColor: ColorConstant.black100,
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(16)),
-      ),
-      content: SizedBox(
-        width: 240,
-        height: 400,
-        child: Column(
-          children: [
-            const Text('誰がAI？'),
-            ...listTiles(),
-            ElevatedButton(
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (_) {
-                    return CorrectDialog(
-                      answerName: memberMap['gpt'].toString(),
-                      isCorrect: value == memberMap['gpt'].toString(),
-                      roomId: roomId,
+          content: SizedBox(
+            width: 240,
+            height: 400,
+            child: Column(
+              children: [
+                const Text('誰がAI？'),
+                ...nameList
+                    .map(
+                      (name) => ListTile(
+                        title: Text(name.toString()),
+                        leading: Radio<String>(
+                          activeColor: ColorConstant.main,
+                          value: name.toString(),
+                          groupValue: value,
+                          onChanged: (String? value) {
+                            if (value != null) {
+                              ref
+                                  .read(answerRadioValueProvider.notifier)
+                                  .update((state) => value);
+                            }
+                          },
+                        ),
+                      ),
+                    )
+                    .toList(),
+                ElevatedButton(
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (_) {
+                        return CorrectDialog(
+                          answerName: data['gpt'].toString(),
+                          isCorrect: value == data['gpt'].toString(),
+                          roomId: roomId,
+                        );
+                      },
                     );
                   },
-                );
-              },
-              style:
-                  ElevatedButton.styleFrom(backgroundColor: ColorConstant.main),
-              child: const Text('決定'),
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: ColorConstant.main),
+                  child: const Text('決定'),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
+      error: (error, stackTrace) {
+        return const Text('エラーが発生しました');
+      },
+      loading: () => const CircularProgressIndicator(),
     );
   }
 }
