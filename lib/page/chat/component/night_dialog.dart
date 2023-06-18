88cb5df944edcb5da8098ea81f10d33b4b30e61d
chouette11 <keifukuda1115@gmail.com>
2023-06-18 17:25:19 +0900

    Fix nightDialog

--- lib/page/chat/component/night_dialog.dart
+++ lib/page/chat/component/night_dialog.dart
@@ -2,54 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:wordwolf/constant/color_constant.dart';
-import 'package:wordwolf/entity/member/member_entity.dart';
 import 'package:wordwolf/page/chat/component/end_dialog.dart';
 import 'package:wordwolf/provider/presentation_providers.dart';
+import 'package:wordwolf/repository/room_repository.dart';
 
-class NightDialog extends ConsumerStatefulWidget {
-  const NightDialog( {
-    super.key,
-    required this.roomId,
-    required this.liveMem,
-    required this.reqUid,
-    required this.maxNum,
-  });
+class NightDialog extends ConsumerWidget {
+  const NightDialog({super.key, required this.roomId});
 
   final String roomId;
-  final List<MemberEntity> liveMem;
-  final String reqUid;
-  final int maxNum;
 
   @override
-  ConsumerState<NightDialog> createState() => _NightDialogState();
-}
-
-class _NightDialogState extends ConsumerState<NightDialog> {
-
-  @override
-  void initState() {
-    super.initState();
-    Future(() async {
-      final uid = ref.read(uidProvider);
-      if (widget.reqUid == uid) {
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final members = ref.watch(membersStreamProvider(widget.roomId));
-    final room = ref.watch(roomStreamProvider(widget.roomId));
+  Widget build(BuildContext context, WidgetRef ref) {
+    final members = ref.watch(membersStreamProvider(roomId));
 
     return members.when(
       data: (data) {
-        final liveMem = [];
-        for (var e in data) {
-          if (e.isLive == true) {
-            liveMem.add(e);
-          }
-        }
+        final liveMem = ref.read(roomRepositoryProvider).getLivingMembers(data);
         WidgetsBinding.instance.addPostFrameCallback((_) {
+          Future.delayed(const Duration(seconds: 5), () {
             if (data[data.indexWhere((e) => e.userId == 'gpt')].isLive ==
                 false) {
               context.pop();
@@ -58,39 +28,31 @@ class _NightDialogState extends ConsumerState<NightDialog> {
                 builder: (context) => EndDialog(result: '村人'),
               );
             }
-            if (data.length <= 2) {
+            if (liveMem.length <= 2) {
               context.pop();
               showDialog(
                 context: context,
                 builder: (context) => EndDialog(result: 'AI'),
               );
             }
-            Future.delayed(Duration(seconds: 5), () {
-            context.pop();
-            context.push("/chat", extra: widget.roomId);
-            });
 
+            context.pop();
+            context.push("/chat", extra: roomId);
+          });
         });
-        return room.when(
-          data: (room) {
-            return AlertDialog(
-              backgroundColor: ColorConstant.black100,
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(16)),
-              ),
-              content: SizedBox(
-                width: 240,
-                height: 400,
-                child: Column(
-                  children: [Text('AIが惨殺しています。しばらくお待ち下さい')],
-                ),
-              ),
-            );
-          },
-          error: (error, stackTrace) {
-            return const Text('エラーが発生しました');
-          },
-          loading: () => const CircularProgressIndicator(),
+
+        return const AlertDialog(
+          backgroundColor: ColorConstant.black100,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(16)),
+          ),
+          content: SizedBox(
+            width: 240,
+            height: 400,
+            child: Column(
+              children: [Text('AIが惨殺しています。しばらくお待ち下さい')],
+            ),
+          ),
         );
       },
       error: (error, stackTrace) {
