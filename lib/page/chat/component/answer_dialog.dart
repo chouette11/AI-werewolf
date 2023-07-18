31e0deba1074562216eefecccd9f64af9357cc1d
chouette11 <keifukuda1115@gmail.com>
2023-07-18 20:13:19 +0900

    Fix logic

--- lib/page/chat/component/answer_dialog.dart
+++ lib/page/chat/component/answer_dialog.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 import 'package:wordwolf/constant/color_constant.dart';
-import 'package:wordwolf/page/chat/component/executed_dialog.dart';
 import 'package:wordwolf/provider/presentation_providers.dart';
 import 'package:wordwolf/repository/room_repository.dart';
 
@@ -32,19 +30,7 @@ class _AnswerDialogState extends ConsumerState<AnswerDialog> {
         return room.when(
           data: (room) {
             // 全員等表示遷移
-            // gptの分を引く
-            if (room.votedSum == livingMem.length - 1) {
-              WidgetsBinding.instance.addPostFrameCallback((_) {
-                context.pop();
-                showDialog(
-                  context: context,
-                  builder: (context) => ExecutedDialog(
-                    roomId: widget.roomId,
-                    members: livingMem,
-                  ),
-                );
-              });
-            }
+
             return AlertDialog(
               backgroundColor: ColorConstant.black100,
               shape: const RoundedRectangleBorder(
