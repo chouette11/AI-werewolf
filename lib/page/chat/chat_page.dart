b22e1f88603d06908056eeafa8fb46b2e0f9c337
chouette11 <keifukuda1115@gmail.com>
2023-07-18 20:11:49 +0900

    Add showExecuteDialog

--- lib/page/chat/chat_page.dart
+++ lib/page/chat/chat_page.dart
@@ -2,10 +2,12 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:wordwolf/entity/room/room_entity.dart';
 import 'package:wordwolf/page/chat/component/answer_dialog.dart';
 import 'package:wordwolf/page/chat/component/bottom_field.dart';
 import 'package:wordwolf/page/chat/component/bottom_text_field.dart';
 import 'package:wordwolf/page/chat/component/chat_appbar.dart';
+import 'package:wordwolf/page/chat/component/executed_dialog.dart';
 import 'package:wordwolf/page/chat/component/receive_message_bubble.dart';
 import 'package:wordwolf/page/chat/component/send_message_bubble.dart';
 import 'package:wordwolf/page/chat/component/theme_dialog.dart';
@@ -13,27 +15,67 @@ import 'package:wordwolf/provider/presentation_providers.dart';
 import 'package:wordwolf/repository/room_repository.dart';
 
 class ChatPage extends ConsumerStatefulWidget {
-  const ChatPage({super.key, required this.roomId});
+  const ChatPage({
+    super.key,
+    required this.roomId,
+    this.isFirst = true,
+  });
+
   final String roomId;
+  final bool isFirst;
 
   @override
   ConsumerState<ChatPage> createState() => _ChatPageState();
 }
 
 class _ChatPageState extends ConsumerState<ChatPage> {
-  int maxNum = 100;
   bool isDialog = false;
 
   Future<void> _showStartDialog() async {
     final room = await ref.read(roomRepositoryProvider).getRoom(widget.roomId);
-    maxNum = room.maxNum;
+    final livingMem = await ref
+        .read(roomRepositoryProvider)
+        .getLivingMembersFromDB(widget.roomId);
+    if (!livingMem.map((e) => e.userId).contains(ref.read(uidProvider))) {
+      return;
+    }
     return showDialog<void>(
       barrierDismissible: false,
       context: context,
       builder: (BuildContext context) {
-        return ThemeDialog(widget.roomId, maxNum);
+        return ThemeDialog(
+          widget.roomId,
+          widget.isFirst ? room.maxNum : livingMem.length,
+        );
       },
     );
+  }
+
+  Future<void> _showExecutedDialog(RoomEntity room, bool isMake) async {
+    final livingMem = await ref
+        .read(roomRepositoryProvider)
+        .getLivingMembersFromDB(widget.roomId);
+    if (!livingMem.map((e) => e.userId).contains(ref.read(uidProvider))) {
+      return;
+    }
+    // gptの分を引く
+    if (room.votedSum == livingMem.length - 1) {
+      if (isMake) {
+        livingMem.sort((a, b) => -a.voted.compareTo(b.voted));
+        final mem = livingMem[0];
+        await ref.read(roomRepositoryProvider).killMember(
+              widget.roomId,
+              mem.userId,
+            );
+      }
+      return showDialog<void>(
+        barrierDismissible: false,
+        context: context,
+        builder: (BuildContext context) {
+          return ExecutedDialog(roomId: room.id, members: livingMem);
+        },
+      );
+    }
   }
 
   @override
@@ -47,6 +89,8 @@ class _ChatPageState extends ConsumerState<ChatPage> {
     final messages = ref.watch(messagesStreamProvider(widget.roomId));
     final counter = ref.watch(limitTimeProvider);
     final uid = ref.watch(uidProvider);
+    final room = ref.watch(roomStreamProvider(widget.roomId));
+    final isMake = ref.watch(isMakeRoomProvider);
 
     if (counter == 0 && !isDialog) {
       WidgetsBinding.instance.addPostFrameCallback((_) {
@@ -59,22 +103,15 @@ class _ChatPageState extends ConsumerState<ChatPage> {
         isDialog = true;
       });
     }
+
+    room.whenData((value) => _showExecutedDialog(value, isMake));
+
     return WillPopScope(
       onWillPop: () async => false,
       child: Scaffold(
         appBar: ChatAppBar(roomId: widget.roomId),
         body: messages.when(
           data: (data) {
-            if (data.length < maxNum) {
-              return SizedBox(
-                height: MediaQuery.of(context).size.height -
-                    MediaQuery.of(context).viewInsets.bottom -
-                    180,
-                child: const Center(
-                  child: Text('それではゲームを開始します！'),
-                ),
-              );
-            }
             return Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
