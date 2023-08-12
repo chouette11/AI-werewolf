32a6710feffc12591564b643de0dedc55fc4b19f
chouette11 <keifukuda1115@gmail.com>
2023-08-12 14:22:16 +0900

    Add flavor

---
+++ lib/environment/src/firebase_options.dart
@@ -0,0 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
+
+import 'firebase_options_dev.dart' as dev;
+import 'firebase_options_prod.dart' as prod;
+import 'flavor.dart';
+
+FirebaseOptions firebaseOptionsWithFlavor(Flavor flavor) {
+  switch (flavor) {
+    case Flavor.dev:
+      return dev.DefaultFirebaseOptions.currentPlatform;
+    case Flavor.stg:
+      return dev.DefaultFirebaseOptions.currentPlatform;
+    case Flavor.prod:
+      return prod.DefaultFirebaseOptions.currentPlatform;
+  }
+}
