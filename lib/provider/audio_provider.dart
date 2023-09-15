2c228acb2573822e4ba67c9a924c58b01ba45cbb
chouette11 <keifukuda1115@gmail.com>
2023-09-15 19:56:20 +0900

    Add title and wait sound provider

--- lib/provider/audio_provider.dart
+++ lib/provider/audio_provider.dart
@@ -5,6 +5,10 @@ final audioProvider = Provider((_) => AudioPlayer());
 
 final audioCacheProvider = Provider((_) => AudioCache());
 
+final titleSoundProvider = StateProvider((ref) => '');
+
+final waitSoundProvider = StateProvider((ref) => '');
+
 final buttonSoundProvider = StateProvider<String>((ref) => '');
 
 final notSoundProvider = StateProvider((ref) => '');
