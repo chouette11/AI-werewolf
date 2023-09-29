4a213a329f6e59d871e68ca7571c1d89b0f7b548
chouette11 <keifukuda1115@gmail.com>
2023-09-30 01:34:18 +0900

    Add user model

---
+++ lib/model/document/user/user_document.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_document.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_UserDocument _$$_UserDocumentFromJson(Map<String, dynamic> json) =>
+    _$_UserDocument(
+      uid: json['uid'] as String,
+    );
+
+Map<String, dynamic> _$$_UserDocumentToJson(_$_UserDocument instance) =>
+    <String, dynamic>{
+      'uid': instance.uid,
+    };
