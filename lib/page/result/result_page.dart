0ef47d50d7743c19b8429fff599144f92916bd4b
chocolate-kuma <natsumi0814okada@gmail.com>
2023-10-03 19:04:20 +0900

    add confetti

--- lib/page/result/result_page.dart
+++ lib/page/result/result_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:ai_werewolf/page/result/component/result_users.dart';
 import 'package:ai_werewolf/page/result/component/back_title_button.dart';
 import 'package:flutter/material.dart';
@@ -10,8 +12,9 @@ import 'package:ai_werewolf/repository/room_repository.dart';
 import 'package:ai_werewolf/util/constant/color_constant.dart';
 import 'package:ai_werewolf/util/constant/text_style_constant.dart';
 import 'package:ai_werewolf/util/enum/role.dart';
+import 'package:confetti/confetti.dart';
 
-class ResultPage extends ConsumerWidget {
+class ResultPage extends ConsumerStatefulWidget {
   const ResultPage({
     Key? key,
     required this.roomId,
@@ -21,8 +24,27 @@ class ResultPage extends ConsumerWidget {
   final String winner;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final members = ref.watch(membersStreamProvider(roomId));
+  ConsumerState<ResultPage> createState() => _ResultPageState();
+}
+
+class _ResultPageState extends ConsumerState<ResultPage> {
+  late ConfettiController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = ConfettiController(duration: const Duration(seconds: 3));
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final members = ref.watch(membersStreamProvider(widget.roomId));
     final uid = ref.watch(uidProvider);
     return Scaffold(
       backgroundColor: ColorConstant.back,
@@ -40,30 +62,58 @@ class ResultPage extends ConsumerWidget {
                 ),
                 const SizedBox(height: 8),
                 Text(
-                  winner == member.role ? '勝利' : '敗北',
+                  widget.winner == member.role ? '勝利' : '敗北',
                   style: TextStyleConstant.bold60,
                 ),
-                Icon(
-                  member.role == RoleEnum.human.displayName
-                      ? Icons.diversity_3
-                      : Icons.psychology_outlined,
-                  color: member.role == RoleEnum.human.displayName
-                      ? ColorConstant.main
-                      : ColorConstant.accent,
-                  size: 152,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: ConfettiWidget(
+                        confettiController: controller,
+                        blastDirection: 0, // radial value - RIGHT
+                        emissionFrequency: 0.1,
+                        numberOfParticles: 10,
+                        gravity: 0,
+                        colors: [Colors.white, Color(0xff74AA9C)],
+                        blastDirectionality: BlastDirectionality
+                            .explosive, // don't specify a direction, blast randomly
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: TextButton(
+                          onPressed: () {
+                            controller.play();
+                          },
+                          child: const Text('singles')),
+                    ),
+                    Icon(
+                      member.role == RoleEnum.human.displayName
+                          ? Icons.diversity_3
+                          : Icons.psychology_outlined,
+                      color: member.role == RoleEnum.human.displayName
+                          ? ColorConstant.main
+                          : ColorConstant.accent,
+                      size: 152,
+                    ),
+                  ],
                 ),
                 const SizedBox(height: 8),
-                ResultUsers(roomId: roomId),
+                ResultUsers(roomId: widget.roomId),
                 const SizedBox(height: 48),
                 BackTitleButton(
                   onTap: () async {
                     await ref
                         .read(messageRepositoryProvider)
-                        .deleteAllMessage(roomId);
-                    await ref.read(memberRepositoryProvider).resetVoted(roomId);
+                        .deleteAllMessage(widget.roomId);
+                    await ref
+                        .read(memberRepositoryProvider)
+                        .resetVoted(widget.roomId);
                     await ref
                         .read(roomRepositoryProvider)
-                        .resetKilledId(roomId);
+                        .resetKilledId(widget.roomId);
                     ref.read(limitTimeProvider.notifier).reset();
                     ref.refresh(answerAssignedIdProvider);
                     context.go('/');
