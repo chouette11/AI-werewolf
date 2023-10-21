ceb0d53d8ead32e5396f027c7d9e4b30e205def7
chouette11 <keifukuda1115@gmail.com>
2023-10-22 02:32:49 +0900

    Delete cofetti

--- lib/page/tutorial/children/tutorial_page_3.dart
+++ lib/page/tutorial/children/tutorial_page_3.dart
@@ -1,10 +1,9 @@
-import 'package:ai_werewolf/page/result/component/back_title_button.dart';
 import 'package:ai_werewolf/page/tutorial/component/tutorial_result_users.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ai_werewolf/util/constant/color_constant.dart';
 import 'package:ai_werewolf/util/constant/text_style_constant.dart';
-import 'package:confetti/confetti.dart';
 
 class TutorialPage3 extends ConsumerStatefulWidget {
   const TutorialPage3({
@@ -18,43 +17,15 @@ class TutorialPage3 extends ConsumerStatefulWidget {
 }
 
 class _TutorialPage3State extends ConsumerState<TutorialPage3> {
-  late ConfettiController controller;
-
-  @override
-  void initState() {
-    super.initState();
-    controller = ConfettiController(duration: const Duration(seconds: 3));
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
-    controller.play();
     return Scaffold(
       backgroundColor: ColorConstant.back,
       body: Center(
         child: Stack(
           alignment: Alignment.topCenter,
           children: [
-            Visibility(
-              visible: widget.isWin,
-              child: ConfettiWidget(
-                confettiController: controller,
-                blastDirection: 0,
-                emissionFrequency: 0.1,
-                numberOfParticles: 10,
-                gravity: 1,
-                colors: const [ColorConstant.black100, ColorConstant.accent],
-                blastDirectionality: BlastDirectionality.explosive,
-                minBlastForce: 10,
-                shouldLoop: true,
-              ),
-            ),
             Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
@@ -75,10 +46,16 @@ class _TutorialPage3State extends ConsumerState<TutorialPage3> {
                 ),
                 const SizedBox(height: 8),
                 const TutorialResultUsers(),
-                const SizedBox(height: 48),
-                BackTitleButton(
-                  onTap: () async {
-                  },
+                const SizedBox(height: 32),
+                AnimatedTextKit(
+                  isRepeatingAnimation: false,
+                  animatedTexts: [
+                    TypewriterAnimatedText(
+                      '今回は3番がAIでした。\n人間陣営はこの3番を処刑することで\n勝利になります。',
+                      textAlign: TextAlign.center,
+                      textStyle: TextStyleConstant.normal16,
+                    ),
+                  ],
                 ),
               ],
             ),
