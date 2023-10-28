47110ec6c88acbcef6d7531b1c5219ba4e1357fb
chocolate-kuma <natsumi0814okada@gmail.com>
2023-10-28 23:30:33 +0900

    change font family

--- lib/util/constant/text_style_constant.dart
+++ lib/util/constant/text_style_constant.dart
@@ -5,97 +5,97 @@ class TextStyleConstant {
   TextStyleConstant._();
 
   static const bold12 = TextStyle(
-      fontFamily: 'NotoSans',
+      fontFamily: 'Kaisei_Decol',
       fontWeight: FontWeight.bold,
       fontSize: 12,
       color: ColorConstant.text);
   static const bold14 = TextStyle(
-      fontFamily: 'NotoSans',
+      fontFamily: 'Kaisei_Decol',
       fontWeight: FontWeight.bold,
       fontSize: 14,
       color: ColorConstant.text);
   static const bold16 = TextStyle(
-      fontFamily: 'NotoSans',
+      fontFamily: 'Kaisei_Decol',
       fontWeight: FontWeight.bold,
       fontSize: 16,
       color: ColorConstant.text);
   static const bold18 = TextStyle(
-      fontFamily: 'NotoSans',
+      fontFamily: 'Kaisei_Decol',
       fontWeight: FontWeight.bold,
       fontSize: 18,
       color: ColorConstant.text);
   static const bold20 = TextStyle(
-      fontFamily: 'NotoSans',
+      fontFamily: 'Kaisei_Decol',
       fontWeight: FontWeight.bold,
       fontSize: 20,
       color: ColorConstant.text);
   static const bold24 = TextStyle(
-      fontFamily: 'NotoSans',
+      fontFamily: 'Kaisei_Decol',
       fontWeight: FontWeight.bold,
       fontSize: 24,
       color: ColorConstant.text);
   static const bold28 = TextStyle(
-      fontFamily: 'NotoSans',
+      fontFamily: 'Kaisei_Decol',
       fontWeight: FontWeight.bold,
       fontSize: 28,
       color: ColorConstant.text);
   static const bold32 = TextStyle(
-      fontFamily: 'NotoSans',
+      fontFamily: 'Kaisei_Decol',
       fontWeight: FontWeight.bold,
       fontSize: 32,
       color: ColorConstant.text);
   static const bold40 = TextStyle(
-      fontFamily: 'NotoSans',
+      fontFamily: 'Kaisei_Decol',
       fontWeight: FontWeight.bold,
       fontSize: 40,
       color: ColorConstant.text);
   static const bold48 = TextStyle(
-      fontFamily: 'NotoSans',
+      fontFamily: 'Kaisei_Decol',
       fontWeight: FontWeight.bold,
       fontSize: 48,
       color: ColorConstant.text);
   static const bold60 = TextStyle(
-      fontFamily: 'NotoSans',
+      fontFamily: 'Kaisei_Decol',
       fontWeight: FontWeight.bold,
       fontSize: 60,
       color: ColorConstant.text);
   static const normal12 = TextStyle(
-      fontFamily: 'NotoSans',
+      fontFamily: 'Kaisei_Decol',
       fontWeight: FontWeight.normal,
       fontSize: 12,
       color: ColorConstant.text);
   static const normal14 = TextStyle(
-      fontFamily: 'NotoSans',
+      fontFamily: 'Kaisei_Decol',
       fontWeight: FontWeight.normal,
       fontSize: 14,
       color: ColorConstant.text);
   static const normal16 = TextStyle(
-      fontFamily: 'NotoSans',
+      fontFamily: 'Kaisei_Decol',
       fontWeight: FontWeight.normal,
       fontSize: 16,
       color: ColorConstant.text);
   static const normal18 = TextStyle(
-      fontFamily: 'NotoSans',
+      fontFamily: 'Kaisei_Decol',
       fontWeight: FontWeight.normal,
       fontSize: 18,
       color: ColorConstant.text);
   static const normal20 = TextStyle(
-      fontFamily: 'NotoSans',
+      fontFamily: 'Kaisei_Decol',
       fontWeight: FontWeight.normal,
       fontSize: 20,
       color: ColorConstant.text);
   static const normal24 = TextStyle(
-      fontFamily: 'NotoSans',
+      fontFamily: 'Kaisei_Decol',
       fontWeight: FontWeight.normal,
       fontSize: 24,
       color: ColorConstant.text);
   static const normal28 = TextStyle(
-      fontFamily: 'NotoSans',
+      fontFamily: 'Kaisei_Decol',
       fontWeight: FontWeight.normal,
       fontSize: 28,
       color: ColorConstant.text);
   static const normal32 = TextStyle(
-      fontFamily: 'NotoSans',
+      fontFamily: 'Kaisei_Decol',
       fontWeight: FontWeight.normal,
       fontSize: 32,
       color: ColorConstant.text);
